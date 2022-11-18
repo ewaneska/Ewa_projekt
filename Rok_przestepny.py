c2148bf2fa97cc7f248a8cac9842877fa39daa5b
Ewa <ewa.neska@pwc.com>
2022-11-18 14:07:37 +0100

    Moje zmiany

--- Rok_przestepny.py
+++ Rok_przestepny.py
@@ -1,7 +1,11 @@
+def rok_przystepny(year):
 
-rok=int(input("Podaj rok:"))
-czy_rok_jest_wielkorotnoscia_4=(rok%4==0)
-wykluczamy_rok_podzielny_na_100=(rok%100!=0)
-wielokrotnosc_400=(rok%400==0)
-czy_przestepny=wielokrotnosc_400 or wykluczamy_rok_podzielny_na_100 and czy_rok_jest_wielkorotnoscia_4
-print("Czy rok", rok, "jest przestepny?", czy_przestepny)
+    czy_przestepny = (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
+
+    print('Czy rok', year, 'jest przestępny?', czy_przestepny)
+
+    return czy_przestepny
+
+if __name__ == '__main__':
+    rok = int(input("Podaj rok"))
+    rok_przystepny(rok)
